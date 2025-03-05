a09bdde7ced5ebeb2c0542a157bef2e86096946a
MD Fahim <fmn.flame@gmail.com>
2025-03-05 20:42:19 +0600

    last update

--- lib/apiclass2.dart
+++ lib/apiclass2.dart
@@ -11,12 +11,29 @@ class Apiclass2 extends StatefulWidget {
 class _Apiclass2State extends State<Apiclass2> {
   final ProductController productControllerObject = ProductController();
 
-  void productDialouge() {
+  mySnackBar(messege, context) {
+    return ScaffoldMessenger.of(context)
+        .showSnackBar(SnackBar(content: Text(messege)));
+  }
+
+  void productDialouge(
+      {String? id,
+      String? name,
+      int? qty,
+      String? img,
+      int? unitPrice,
+      int? totalPrice}) {
     TextEditingController nameController = TextEditingController();
     TextEditingController imageController = TextEditingController();
     TextEditingController qtyController = TextEditingController();
     TextEditingController unitPriceController = TextEditingController();
     TextEditingController totalPriceController = TextEditingController();
+
+    nameController.text = name ?? '';
+    qtyController.text = qty.toString() ?? '0';
+    imageController.text = img ?? '';
+    unitPriceController.text = unitPrice.toString() ?? '0';
+    totalPriceController.text = totalPrice.toString() ?? '0';
 
     showDialog(
       context: context,
@@ -24,7 +41,7 @@ class _Apiclass2State extends State<Apiclass2> {
         return AlertDialog(
           scrollable: true,
           shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-          title: const Text("Add Product"),
+          title: Text((id == null) ? "Add Product" : "Edit product"),
           content: SingleChildScrollView(
             child: Column(
               children: [
@@ -68,7 +85,7 @@ class _Apiclass2State extends State<Apiclass2> {
                 const SizedBox(width: 20),
                 ElevatedButton.icon(
                   onPressed: () {
-                    setState(() {
+                    if (id == null) {
                       productControllerObject.createProduct(
                         nameController.text,
                         imageController.text,
@@ -76,15 +93,26 @@ class _Apiclass2State extends State<Apiclass2> {
                         int.parse(unitPriceController.text),
                         int.parse(totalPriceController.text),
                       );
-                      fetchData();
-
-                      Navigator.pop(context);
-                    });
-
+                    } else {
+                      productControllerObject.updateProduct(
+                        id,
+                        nameController.text,
+                        imageController.text,
+                        int.parse(qtyController.text),
+                        int.parse(unitPriceController.text),
+                        int.parse(totalPriceController.text),
+                      );
+                    }
+                    //fetchData();
                     setState(() {});
+
+                    Navigator.pop(context);
+                    Future.delayed(Duration(milliseconds: 500), () {
+                      fetchData();
+                    });
                   },
                   icon: const Icon(Icons.add, color: Colors.white),
-                  label: const Text("Add Product"),
+                  label: Text((id == null) ? "Add Product" : "update product"),
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.black,
                     foregroundColor: Colors.white,
@@ -101,9 +129,10 @@ class _Apiclass2State extends State<Apiclass2> {
   }
 
   Future<void> fetchData() async {
-    await productControllerObject.fetechProducts(); // Fetch updated products
-
-    setState(() {});
+    print("Fetching data...");
+    await productControllerObject.fetechProducts();
+    print("Data fetched: ${productControllerObject.products}");
+    setState(() {}); // Ensure UI updates
   }
 
   @override
@@ -152,10 +181,33 @@ class _Apiclass2State extends State<Apiclass2> {
                         mainAxisSize: MainAxisSize.min,
                         children: [
                           IconButton(
-                              onPressed: () => productDialouge(),
+                              onPressed: () => productDialouge(
+                                    id: product.sId,
+                                    name: product.productName,
+                                    img: product.img,
+                                    qty: product.qty,
+                                    unitPrice: product.unitPrice,
+                                    totalPrice: product.totalPrice,
+                                  ),
                               icon: const Icon(Icons.edit)),
                           IconButton(
-                              onPressed: () {}, icon: const Icon(Icons.delete)),
+                              onPressed: () {
+                                productControllerObject
+                                    .deleteProduct(product.sId.toString())
+                                    .then((value) {
+                                  if (value) {
+                                    setState(() {
+                                      fetchData();
+                                    });
+                                    mySnackBar("Producte deleted", context);
+                                  } else {
+                                    mySnackBar("Something went wrong", context);
+                                  }
+                                });
+
+                                setState(() {});
+                              },
+                              icon: const Icon(Icons.delete)),
                         ],
                       ),
                     ),
--- lib/utils/product_controller.dart
+++ lib/utils/product_controller.dart
@@ -2,36 +2,69 @@ import 'dart:convert';
 
 import 'package:api_concepts/utils/urls.dart';
 import 'package:http/http.dart' as http;
-import 'package:api_concepts/model/product.dart';
+import 'package:api_concepts/model/product_model.dart';
 
 class ProductController {
   List<Data> products = [];
-  
+
   Future<void> fetechProducts() async {
     final response = await http.get(Uri.parse(Urls.readProduct));
 
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
-      products = data['data'];
+
+      ProductModel productModel = ProductModel.fromJson(data);
+      products = productModel.data ?? [];
     }
   }
 
-  Future<void> createProduct(String name,String img,int qty,int price,int totalPrice) async {
+  Future<void> createProduct(
+      String name, String img, int qty, int price, int totalPrice) async {
     final response = await http.post(Uri.parse(Urls.createProduct),
-    headers: {'Content-Type' : 'application/json'},
-      body: jsonEncode({
-        "ProductName": name,
-        "ProductCode": DateTime.now().microsecondsSinceEpoch,
-        "Img": img,
-        "Qty": qty,
-        "UnitPrice": price,
-        "TotalPrice": totalPrice
-      })
-    );
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          "ProductName": name,
+          "ProductCode": DateTime.now().microsecondsSinceEpoch,
+          "Img": img,
+          "Qty": qty,
+          "UnitPrice": price,
+          "TotalPrice": totalPrice
+        }));
 
     // print(response.body.toString());
     if (response.statusCode == 201) {
       await fetechProducts();
     }
   }
+
+  Future<void> updateProduct(String id, String name, String img, int qty,
+      int price, int totalPrice) async {
+    final response = await http.post(Uri.parse(Urls.updateProduct(id)),
+        headers: {'Content-Type': 'application/json'},
+        body: jsonEncode({
+          "ProductName": name,
+          "ProductCode": DateTime.now().microsecondsSinceEpoch,
+          "Img": img,
+          "Qty": qty,
+          "UnitPrice": price,
+          "TotalPrice": totalPrice
+        }));
+
+    // print(response.body.toString());
+    if (response.statusCode == 201) {
+      await fetechProducts();
+    }
+  }
+
+  Future<bool> deleteProduct(String id) async {
+    final response = await http.get(Uri.parse(Urls.deleteProduct(id)));
+
+    // print(response.body.toString());
+    if (response.statusCode == 200) {
+      return true;
+    }
+    return false;
+  }
+
+  // end of the code
 }
